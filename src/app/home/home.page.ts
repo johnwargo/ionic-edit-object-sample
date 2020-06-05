bda3c8f994d2af3449095caeed3f0665168c228b
John M. Wargo <johnwargo@users.noreply.github.com>
2020-06-05 16:33:32 -0400

    Updated home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+class DataObject {
+  field1: string;
+  field2: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dataObject: DataObject = {
+    field1: 'Some random value',
+    field2: 'Another random value',
+  };
+
+  constructor() {
+    console.log('HomePage: constructor()');
+  }
+
+  openRepo() {
+    console.log('HomePage: openRepo()');
+    window.open('https://github.com/johnwargo/ionic-edit-object-sample');
+  }
+
+  editNoCancel() {
+    console.log('HomePage: editNoCancel()');
+
+  }
+
+  editWithCancel() {
+    console.log('HomePage: editWithCancel()');
+
+  }
 
 }
