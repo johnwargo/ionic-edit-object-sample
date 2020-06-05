6ecf98e57265d5441b2f317da6c43578d0e07bf8
John M. Wargo <johnwargo@users.noreply.github.com>
2020-06-05 19:31:25 -0400

    completed the app

--- src/app/edit-cancel/edit-cancel.page.ts
+++ src/app/edit-cancel/edit-cancel.page.ts
@@ -1,8 +1,9 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { DataObject } from '../classes/data-object';
-
+import clone from '../utils';
 
 @Component({
   selector: 'app-edit-cancel',
@@ -12,9 +13,11 @@ import { DataObject } from '../classes/data-object';
 export class EditCancelPage implements OnInit {
 
   dataObject: DataObject;
+  tempObject: DataObject;
 
   constructor(
     private route: ActivatedRoute,
+    private navCtrl: NavController,
     private router: Router
   ) {
     console.log('EditCancelPage: constructor()');
@@ -23,16 +26,27 @@ export class EditCancelPage implements OnInit {
   ngOnInit() {
     console.log('EditCancelPage: ngOnInit()');
     this.route.queryParams.subscribe(params => {
+      // Pull state off the router
       const state = this.router.getCurrentNavigation().extras.state;
+      // Get the data object off the state
       this.dataObject = state.dataObject;
+      // Clone it to work with it locally
+      this.tempObject = clone(this.dataObject);
     });
   }
 
   dismiss() {
-
+    console.log('EditCancelPage: dismiss()');
+    // Pop the page off the stack
+    this.navCtrl.pop();
   }
 
   save() {
-
+    console.log('EditCancelPage: save()');
+    // Have to directly modify the properties to get Ionic to pass the values back
+    this.dataObject.field1 = this.tempObject.field1;
+    this.dataObject.field2 = this.tempObject.field2;
+    // Pop the page off the stack
+    this.navCtrl.pop();
   }
 }
